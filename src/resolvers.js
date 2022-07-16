1202251a0c44b0c2f5118f31edeffee6d0db379d
remmi755 <90457721+remmi755@users.noreply.github.com>
2022-07-16 16:57:43 +0300

    'Hi

--- src/resolvers.js
+++ src/resolvers.js
@@ -14,7 +14,13 @@ const resolvers = {
     module: (_, { id }, { dataSources }) => {
       return dataSources.trackAPI.getModule(id);
     },
+    durationInSeconds: ({ length }) => length,
   },
+
+  Module: {
+    durationInSeconds: ({ length }) => length,
+  },
+
   Mutation: {
     // increments a track's numberOfViews property
     incrementTrackViews: async (_, { id }, { dataSources }) => {
